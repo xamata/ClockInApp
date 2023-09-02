d827bf5ad60996566550973f27fecc36c6df1880
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-02 04:09:40 -0700

    Extract SqlDataAccess Interface
    Interface of the SqlDataAccess class extracted for dependency injection.

--- ClockInAppLibrary/Databases/SqlDataAccess.cs
+++ ClockInAppLibrary/Databases/SqlDataAccess.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 
 namespace ClockInAppLibrary.Database
 {
-    public class SqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
         private readonly IConfiguration _config;
 

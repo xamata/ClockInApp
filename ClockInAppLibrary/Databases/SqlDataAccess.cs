a3d7bc7eb9515e392b4a5cfc17e785906993b996
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-02 04:07:27 -0700

    SqlDataAccess Setup
    All code and packages were added to finish the SqlDataAccess.

--- ClockInAppLibrary/Databases/SqlDataAccess.cs
+++ ClockInAppLibrary/Databases/SqlDataAccess.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClockInAppLibrary.Database
 {
     public class SqlDataAccess
     {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+        public List<T> LoadData<T, U>(string sqlStatement,
+                                     U parameter,
+                                     string connectionStringName,
+                                     bool isStoredProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameter, commandType: commandType).ToList();
+                return rows;
+            }
+        }
+        public void SaveData<T>(string sqlStatement,
+                                T parameter,
+                                string connectionStringName,
+                                bool isStoredProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameter, commandType: commandType);
+            }
+        }
     }
 }

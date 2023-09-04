6175fc255c883f19a5e9b59a2fc7712774ddacf0
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-04 13:54:31 -0700

    Login Page Linked to Portal Page
    The Login page has been linked to the newly created Portal page. The next step is adding a varying features to the portal dependent on the employee's ClockedIn status.

--- ClockInApp.Web/Pages/EmployeeLogin.cshtml.cs
+++ ClockInApp.Web/Pages/EmployeeLogin.cshtml.cs
@@ -1,3 +1,5 @@
+using ClockInAppLibrary.Models;
+using ClockInAppLibrary.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +7,21 @@ namespace ClockInApp.Web.Pages
 {
     public class EmployeeLoginModel : PageModel
     {
+        private readonly IDatabaseData _db;
+
+        [BindProperty]
         public string EmployeePin { get; set; }
+        public EmployeeModel Employee { get; set; }
+        public EmployeeLoginModel(IDatabaseData db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+
         }
+        //public IActionResult OnPost()
+        //{
+        //}
     }
 }
---
+++ ClockInApp.Web/Pages/EmployeePortal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClockInApp.Web.Pages
+{
+    public class EmployeePortalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

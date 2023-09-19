a7e9c2614a0423ef4bc6beb697efa6f1bebd70a0
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-18 22:58:02 -0700

    Fixed ClockIn Stored Procedure
    Now, when the clock in button is pressed, the IsClockedIn status is from 0 to 1 in the Employee's Table.

--- ClockInDb/dbo/Stored Procedures/spShiftTimes_ClockedIn.sql
+++ ClockInDb/dbo/Stored Procedures/spShiftTimes_ClockedIn.sql
@@ -3,12 +3,7 @@
 AS
 begin
 	set nocount on;
-
-	if not exists(select 1 from dbo.Employees where Id = @employeeId)
-	begin
-		update dbo.Employees
-		set IsClockedIn = 1
-		where Id = @employeeId;
-	end
-
+	update dbo.Employees
+	set IsClockedIn = 1
+	where Id = @employeeId;
 end

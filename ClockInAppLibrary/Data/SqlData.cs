1a3b2ba7f551130904bc169377b886a45bd8bc41
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-26 14:27:30 -0700

    Tidying SQL Data Class

--- ClockInAppLibrary/Data/SqlData.cs
+++ ClockInAppLibrary/Data/SqlData.cs
@@ -18,13 +18,6 @@ namespace ClockInAppLibrary.Data
         {
             _db = db;
         }
-        //public List<EmployeeModel> GetEmployeesPin()
-        //{
-        //    return _db.LoadData<EmployeeModel, dynamic>("dbo.spEmployees_GetAllPins",
-        //                                        new { },
-        //                                        connectionStringName,
-        //                                        true);
-        //}
 
         public EmployeeModel LoginToPortal(string ePin)
         {
@@ -62,68 +55,5 @@ namespace ClockInAppLibrary.Data
                true);
         }
 
-        //// Create a new booking
-        //public void BookGuest(string firstName,
-        //                      string lastName,
-        //                      DateTime startDate,
-        //                      DateTime endDate,
-        //                      int roomTypeId)
-        //{
-
-        //    GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
-        //        new { firstName, lastName },
-        //        connectionStringName,
-        //        true).First();
-
-        //    // Getting the total cost
-        //    RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
-        //        new { Id = roomTypeId },
-        //        connectionStringName,
-        //        false).First();
-
-        //    TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
-
-
-        //    List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
-        //        new { startDate, endDate, roomTypeId },
-        //                    connectionStringName,
-        //                    true);
-
-        //    _db.SaveData("dbo.spBookings_Insert",
-        //        new
-        //        {
-        //            roomId = availableRooms.First().Id,
-        //            guestId = guest.Id,
-        //            startDate,
-        //            endDate,
-        //            totalCost = (timeStaying.Days * roomType.Price)
-        //        },
-        //        connectionStringName,
-        //        true);
-        //}
-
-        //public List<BookingFullModel> SearchBookings(string lastName)
-        //{
-        //    return _db.LoadData<BookingFullModel, dynamic>("dbo.spBookings_Search",
-        //        new { lastName, startDate = DateTime.Now.Date },
-        //        connectionStringName,
-        //        true);
-        //}
-
-        //public void CheckInGuest(int bookingId)
-        //{
-        //    _db.SaveData("dbo.spBookings_CheckIn",
-        //        new { Id = bookingId },
-        //        connectionStringName,
-        //        true);
-        //}
-
-        //public RoomTypeModel GetRoomTypeById(int id)
-        //{
-        //    return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetById",
-        //        new { id },
-        //        connectionStringName,
-        //        true).FirstOrDefault();
-        //}
     }
 }
